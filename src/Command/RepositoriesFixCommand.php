11e08b43035d8e1ebb1a713d68ea05ea746aeb9e
Rostyslav Proskuriakov <proskuryakov.r@gmail.com>
2020-04-09 01:43:50 +0200

    Implement --path and --fixer

--- src/Command/RepositoriesFixCommand.php
+++ src/Command/RepositoriesFixCommand.php
@@ -25,37 +25,67 @@ class RepositoriesFixCommand extends Command
     protected function configure(): void
     {
         $this
-            ->addOption('fixer', null, InputOption::VALUE_OPTIONAL, 'Apply only specified fixer')
-            ->addOption('path', null, InputOption::VALUE_OPTIONAL, 'Run command on specified path only')
+            ->addOption(
+                'fixer',
+                null,
+                InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY,
+                'Apply only specified fixer'
+            )
+            ->addOption(
+                'path',
+                null,
+                InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY,
+                'Run command on specified path only'
+            )
             ->setDescription('Setting up repositories specified in repos.yaml, applying fixers for them');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        try {
+            if ($input->getOption('path') !== []) {
+                $this->multiRepositoryHandler->replaceRepositories($input->getOption('path'));
+            }
+
+            if ($input->getOption('fixer') !== []) {
+                $this->multiRepositoryHandler->replaceFixers($input->getOption('fixer'));
+            }
+        } catch (Throwable $exception) {
+            return $this->handleException(
+                $output,
+                $exception,
+                sprintf('Got error during initialization: %s', $exception->getMessage())
+            );
+        }
+
         $total = $this->multiRepositoryHandler->getRepositoriesCount();
         $i = 0;
 
         try {
             foreach ($this->multiRepositoryHandler->iterateHandle() as $repoName) {
-                $output->write(
-                    sprintf('%04d / %04d Repository %s fixed successfully', ++$i, $total, $repoName),
-                    true
+                $output->writeln(
+                    sprintf('<fg=green>%04d / %04d Repository %s fixed successfully</>', ++$i, $total, $repoName)
                 );
             }
         } catch (Throwable $exception) {
-            $output->write(
+            return $this->handleException(
+                $output,
+                $exception,
                 sprintf(
                     '<error>%04d / %04d Repository failed to fix with message: %s</error>',
                     ++$i,
                     $total,
                     $exception->getMessage()
-                ),
-                true
-            );
-
-            return 1;
+                ));
         }
 
         return 0;
+    }
+
+    private function handleException(OutputInterface $output, Throwable $exception, string $message): int
+    {
+        $output->writeln($message);
+
+        return $exception->getCode() ?? 1;
     }
 }
--- src/Handler/MultiRepositoryHandler.php
+++ src/Handler/MultiRepositoryHandler.php
@@ -3,6 +3,7 @@
 namespace Linkorb\MultiRepo\Handler;
 
 use Generator;
+use InvalidArgumentException;
 use Linkorb\MultiRepo\Dto\RepoInputDto;
 
 class MultiRepositoryHandler
@@ -35,5 +36,48 @@ class MultiRepositoryHandler
     public function getRepositoriesCount(): int
     {
         return count($this->config['list']);
+    }
+
+    /**
+     * @param string[] $repositories
+     */
+    public function replaceRepositories(array $repositories): void
+    {
+        $currentRepoList = $this->config['list'];
+        $this->config['list'] = [];
+
+        foreach ($repositories as $repositoryPath) {
+            $repoPathComponents = explode(DIRECTORY_SEPARATOR, $repositoryPath);
+            $repoName = end($repoPathComponents);
+
+            if ($repoName === false) {
+                throw new InvalidArgumentException('Wrong repository path passed');
+            }
+
+            if (!array_key_exists($repoName, $currentRepoList)) {
+                throw new InvalidArgumentException(sprintf('Passed repository `%s` doesn\'t exists in config', $repoName));
+            }
+
+            $this->config['list'][$repoName] = $currentRepoList[$repoName];
+        }
+    }
+
+    /**
+     * @param string[] $fixers
+     */
+    public function replaceFixers(array $fixers): void
+    {
+        if ($this->config['defaults']['fixers'] ?? false) {
+            $this->config['defaults']['fixers'] = array_intersect_key($fixers, $this->config['defaults']['fixers']);
+        }
+
+        foreach ($this->config['list'] as $repoName => $repoDsn) {
+            if ($this->config['configs'][$repoName]['fixers'] ?? false) {
+                $this->config['configs'][$repoName]['fixers'] = array_intersect_key(
+                    $fixers,
+                    $this->config['configs'][$repoName]['fixers']
+                );
+            }
+        }
     }
 }
